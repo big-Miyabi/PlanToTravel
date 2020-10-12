61cf98c29d7301c22c12c1f70ea8a18277a9ac78
so-so-so-so <57979322+so-so-so-so@users.noreply.github.com>
2020-10-12 15:31:45 +0900

    Add: maigrateファイル作成/編集

---
+++ app/Schedules_tag.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Schedules_tag extends Model
+{
+    //
+}
---
+++ database/migrations/2020_10_12_062632_create_likes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+          $table->increments("id");
+        $table->unsignedInteger('uid');
+        $table->unsignedInteger('schedule_id');
+        $table->timestamps();
+        $table->foreign('uid')->references('id')->on('users');
+        $table->foreign('schedule_id')->references('id')->on('schedules');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
