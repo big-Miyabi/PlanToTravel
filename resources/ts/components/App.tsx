5d9b94d57f2790f26ea0122b87706d0677587979
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-12 11:29:06 +0900

    Add: react-routerとそのテスト処理を追加

--- resources/ts/components/App.tsx
+++ resources/ts/components/App.tsx
@@ -1,10 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { FC } from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-const App = () => {
-    return <p>表示されてる？</p>;
-};
+import GlobalNav from './GlobalNav';
+import Top from './Top';
+import About from './About';
 
-if (document.getElementById("app")) {
-    ReactDOM.render(<App />, document.getElementById("app"));
+const App: FC = () => {
+    return(
+        <BrowserRouter>
+            <GlobalNav />
+　　　　　　　　　　{/*完全一致のため、exactを付与*/}
+                <Route path="/" exact component={Top} />
+                <Route path="/about" component={About} />
+        </BrowserRouter>
+    )
+}
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<App />, document.getElementById('app'));
 }
---
+++ resources/ts/components/Top.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const Top = () => {
+    return <h1>Top</h1>
+}
+
+export default Top;
