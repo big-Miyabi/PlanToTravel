bcf5d181f5475fbe7ab6f0febb530731d6e87abb
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-12 12:32:31 +0900

    Fix: eslintの設定に誤りがあったので修正した

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,6 @@ const mix = require("laravel-mix");
  */
 
 mix.ts("resources/ts/app.tsx", "public/js").sass(
-    "resources/sass/app.scss",
-    "public/css"
+  "resources/sass/app.scss",
+  "public/css"
 );
