29b510d191ded2f6af7d8f808a94c78179df8788
so-so-so-so <57979322+so-so-so-so@users.noreply.github.com>
2020-10-13 15:25:15 +0900

    Remove: 余分なmigrate削除 Fix: Register.contorollerの編集 nameをusernameに変更

--- resources/ts/tests/App.tsx
+++ resources/ts/tests/App.tsx
@@ -1,22 +1,22 @@
-import React, { FC } from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React, { FC } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import GlobalNav from './GlobalNav';
-import Top from './Top';
-import About from './About';
+import GlobalNav from "./GlobalNav";
+import Top from "./Top";
+import About from "./About";
 
 const App: FC = () => {
-  return(
+  return (
     <BrowserRouter>
       <GlobalNav />
       {/*完全一致のため、exactを付与*/}
       <Route path="/" exact component={Top} />
       <Route path="/about" component={About} />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-if (document.getElementById('app')) {
-  ReactDOM.render(<App />, document.getElementById('app'));
+if (document.getElementById("app")) {
+  ReactDOM.render(<App />, document.getElementById("app"));
 }
