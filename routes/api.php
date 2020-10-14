bea4621b0aff5d470acd5c6f92ce24fb33a7892c
so-so-so-so <57979322+so-so-so-so@users.noreply.github.com>
2020-10-14 10:17:42 +0900

    Add: User.phpにiconとheaderの追加 UserController.phpにiconとheaderを追加　api.phpにAuthのrouteを追加 Fix: Register.tsxをいろいろ修正

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
+});
+Route::group(['middleware' => 'api'], function () {
+  Auth::routes();
 });
--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,11 @@ use Illuminate\Support\Facades\Auth;
 //   return view('welcome');
 // });
 
-Route::get('/{any}', function () {
-  return view('App');
-})->where('any', '.*'); // .*は、正規表現で0文字以上の任意の文字列を意味する
-
+// Route::get('/{any}', function () {
+//   return view('auth/App');
+// })->where('any', '.*'); // .*は、正規表現で0文字以上の任意の文字列を意味する
 Auth::routes();
-
+Route::get('/', function () {
+  return view('welcome');
+});
 Route::get('/home', 'HomeController@index')->name('home');
