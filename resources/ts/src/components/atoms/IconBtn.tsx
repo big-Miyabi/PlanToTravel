51acb5cd085075317744943b182ea9ae96512674
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-14 11:38:16 +0900

    Add: アイコンコンポーネントを作成してpropsでアイコンを指定できるようにした

--- resources/ts/src/components/atoms/IconBtn.tsx
+++ resources/ts/src/components/atoms/IconBtn.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from "@fortawesome/fontawesome-common-types"
 
-const IconBtn: FC = () => {
+type Props = {
+  icon: IconDefinition,
+}
+
+const IconBtn: FC<Props> = ({ icon }) => {
   return (
-    <div>
-    </div>
+    <FontAwesomeIcon icon={icon} />
   )
 }
 
