24602b1c6bb006360ce68744dff0bfcbb677226e
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-14 11:38:16 +0900

    Add: アイコンコンポーネントを作成してpropsでアイコンを指定できるようにした

--- resources/ts/src/screens/RegisterScreen.tsx
+++ resources/ts/src/screens/RegisterScreen.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from "react"
 
 import IconBtn from "../components/atoms/IconBtn"
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 const RegisterScreen: FC = () => {
   return (
     <div className="register">
-      <IconBtn />
+      <IconBtn icon={faHome} />
       {/* <RegistForm /> */}
     </div>
   )
