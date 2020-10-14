c75b9cc913e550d306e4cd78880bd9f54223a0fa
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-14 15:48:09 +0900

    Fix: Auth::route()をコメントアウトした

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,10 @@ use Illuminate\Support\Facades\Auth;
 //   return view('welcome');
 // });
 
-// Route::get('/{any}', function () {
-//   return view('app');
-// })->where('any', '.*'); // .*は、正規表現で0文字以上の任意の文字列を意味する
+Route::get('/{any}', function () {
+  return view('app');
+})->where('any', '.*'); // .*は、正規表現で0文字以上の任意の文字列を意味する
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
