27713be2aef17a08a8b83fb2a2280ce37adc7d6c
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-15 15:18:04 +0900

    Add: メニューボタン押下時に表示されるログインメニューを追加

---
+++ resources/ts/src/containers/organisms/UserMenu.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import UserMenu from '../../components/organisms/UserMenu'
+
+const UserMenuContainer: FC = () => {
+  return <UserMenu />
+}
+
+export default UserMenuContainer
--- resources/ts/src/screens/HomeScreen.tsx
+++ resources/ts/src/screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react'
 import Header from '../containers/organisms/Header'
+import UserMenu from '../containers/organisms/UserMenu'
 
 const HomeScreen: FC = () => {
   return (
     <div className="home">
       <Header />
+      <UserMenu />
       {/* <PopularPosts />
       <NewPosts />
       <PostBtn /> */}
