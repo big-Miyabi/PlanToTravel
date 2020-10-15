01b733707caa57fe84e9f31ee4bdf576b9d45a98
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-15 14:32:41 +0900

    Add: Home画面にヘッダーを追加した

--- public/js/app.js
+++ public/js/app.js
@@ -83362,11 +83362,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
 var react_fontawesome_1 = __webpack_require__(/*! @fortawesome/react-fontawesome */ "./node_modules/@fortawesome/react-fontawesome/index.es.js");
-var IconBtn = function (_a) {
+var FontAwesomeIconBtn = function (_a) {
     var icon = _a.icon, className = _a.className, onClick = _a.onClick;
     return (react_1.default.createElement(react_fontawesome_1.FontAwesomeIcon, { onClick: onClick, className: className, icon: icon }));
 };
-exports.default = IconBtn;
+exports.default = FontAwesomeIconBtn;
 
 
 /***/ }),
@@ -83410,11 +83410,36 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
 var colors_1 = __webpack_require__(/*! ../../../utilities/colors */ "./resources/ts/src/utilities/colors.ts");
 var NaviIcon = function (_a) {
-    var _b = _a.color, color = _b === void 0 ? colors_1.colors.yellow : _b;
-    return (react_1.default.createElement("svg", { width: "23", height: "28", viewBox: "0 0 23 28", fill: "none", xmlns: "http://www.w3.org/2000/svg" },
+    var className = _a.className, _b = _a.color, color = _b === void 0 ? colors_1.colors.yellow : _b;
+    return (react_1.default.createElement("svg", { className: className, width: "23", height: "28", viewBox: "0 0 23 28", fill: "none", xmlns: "http://www.w3.org/2000/svg" },
         react_1.default.createElement("path", { d: "M2 10L11.5 19L21 10", stroke: color, "stroke-width": "3", "stroke-linecap": "round", "stroke-linejoin": "round" })));
 };
 exports.default = NaviIcon;
+
+
+/***/ }),
+
+/***/ "./resources/ts/src/components/atoms/svg/SearchIcon.tsx":
+/*!**************************************************************!*\
+  !*** ./resources/ts/src/components/atoms/svg/SearchIcon.tsx ***!
+  \**************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
+var colors_1 = __webpack_require__(/*! ../../../utilities/colors */ "./resources/ts/src/utilities/colors.ts");
+var SearchIcon = function (_a) {
+    var className = _a.className, _b = _a.color, color = _b === void 0 ? colors_1.colors.yellow : _b;
+    return (react_1.default.createElement("svg", { className: className, width: "22", height: "25", viewBox: "0 0 22 25", fill: "none", xmlns: "http://www.w3.org/2000/svg" },
+        react_1.default.createElement("path", { d: "M21.6992 20.7852L17.4023 16.4883C17.1875 16.3164 16.9297 16.1875 16.6719 16.1875H15.9844C17.1445 14.6836 17.875 12.793 17.875 10.6875C17.875 5.78906 13.8359 1.75 8.9375 1.75C3.99609 1.75 0 5.78906 0 10.6875C0 15.6289 3.99609 19.625 8.9375 19.625C11 19.625 12.8906 18.9375 14.4375 17.7344V18.4648C14.4375 18.7227 14.5234 18.9805 14.7383 19.1953L18.9922 23.4492C19.4219 23.8789 20.0664 23.8789 20.4531 23.4492L21.6562 22.2461C22.0859 21.8594 22.0859 21.2148 21.6992 20.7852ZM8.9375 16.1875C5.88672 16.1875 3.4375 13.7383 3.4375 10.6875C3.4375 7.67969 5.88672 5.1875 8.9375 5.1875C11.9453 5.1875 14.4375 7.67969 14.4375 10.6875C14.4375 13.7383 11.9453 16.1875 8.9375 16.1875Z", fill: color })));
+};
+exports.default = SearchIcon;
 
 
 /***/ }),
@@ -83540,11 +83565,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
 var colors_1 = __webpack_require__(/*! ../../utilities/colors */ "./resources/ts/src/utilities/colors.ts");
 var NaviIcon_1 = __importDefault(__webpack_require__(/*! ../atoms/svg/NaviIcon */ "./resources/ts/src/components/atoms/svg/NaviIcon.tsx"));
+var SearchIcon_1 = __importDefault(__webpack_require__(/*! ../atoms/svg/SearchIcon */ "./resources/ts/src/components/atoms/svg/SearchIcon.tsx"));
 // type Props = {}
 var Header = function () {
-    return (react_1.default.createElement("div", null,
-        react_1.default.createElement(NaviIcon_1.default, { color: colors_1.colors.lightGray }),
-        react_1.default.createElement("p", null, "aaaaa")));
+    return (react_1.default.createElement("div", { className: "header" },
+        react_1.default.createElement(NaviIcon_1.default, { className: "header__menu", color: colors_1.colors.lightGray }),
+        react_1.default.createElement("h1", { className: "header__logo-wrap" },
+            react_1.default.createElement("img", { className: "header__logo", alt: "PLAN TO TRAVEL", src: "../images/logo_yoko.png" })),
+        react_1.default.createElement(SearchIcon_1.default, { className: "header__search", color: colors_1.colors.lightGray })));
 };
 exports.default = Header;
 
@@ -83675,7 +83703,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
 var Header_1 = __importDefault(__webpack_require__(/*! ../containers/organisms/Header */ "./resources/ts/src/containers/organisms/Header.tsx"));
 var HomeScreen = function () {
-    return (react_1.default.createElement(react_1.default.Fragment, null,
+    return (react_1.default.createElement("div", { className: "home" },
         react_1.default.createElement(Header_1.default, null)));
 };
 exports.default = HomeScreen;
--- resources/ts/src/screens/HomeScreen.tsx
+++ resources/ts/src/screens/HomeScreen.tsx
@@ -3,12 +3,12 @@ import Header from '../containers/organisms/Header'
 
 const HomeScreen: FC = () => {
   return (
-    <>
+    <div className="home">
       <Header />
       {/* <PopularPosts />
       <NewPosts />
       <PostBtn /> */}
-    </>
+    </div>
   )
 }
 
