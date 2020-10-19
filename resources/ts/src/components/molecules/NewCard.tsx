8a50c8835bfa630d244cdb480e991efd318c863f
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-19 16:33:56 +0900

    Add: 新着の投稿を一列2投稿ずつ表示する処理を追加

---
+++ resources/ts/src/components/molecules/NewCard.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from 'react'
+import GoToLogo from '../atoms/svg/GoToLogo'
+import FontAwesomeIconBtn from '../atoms/FontAwesomeIconBtn'
+import ItineraryInCard from '../molecules/ItineraryInCard'
+import {
+  faHeart,
+  faBookmark,
+} from '@fortawesome/free-solid-svg-icons'
+import { PostCardType } from '../../utilities/types'
+
+type Props = {
+  post: PostCardType
+}
+
+const NewCard: FC<Props> = ({ post }) => {
+  const goTostyle = post.hasGoTo ? {} : { display: 'none' }
+  const bgStyle = {
+    backgroundImage: 'url(' + post.header + ')',
+  }
+
+  return (
+    <div className="new-card" style={bgStyle}>
+      <div style={goTostyle}>
+        <GoToLogo className="new-card__go-to" />
+      </div>
+      <FontAwesomeIconBtn
+        className="new-card__bookmar"
+        icon={faBookmark}
+      />
+      <div className="new-card__favorite">
+        <FontAwesomeIconBtn
+          className="new-card__favorite-btn"
+          icon={faHeart}
+        />
+        <p className="new-card__favorite-number">
+          {post.favNum}
+        </p>
+      </div>
+      <ItineraryInCard
+        className="new-card__itineray"
+        itinerary={post.itinerary}
+      />
+    </div>
+  )
+}
+
+export default NewCard
