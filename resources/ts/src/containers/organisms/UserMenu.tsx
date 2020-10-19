1fd5bf041aa71c5aa9f1330844e2c122d09264f0
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-19 10:20:11 +0900

    Fix: eslint+prettierでSwitch文のインデントがおかしくなっていたので修正した

--- resources/ts/src/containers/organisms/UserMenu.tsx
+++ resources/ts/src/containers/organisms/UserMenu.tsx
@@ -1,10 +1,14 @@
 import React, { FC, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../reducers/index'
 import UserMenu from '../../components/organisms/UserMenu'
 
 const UserMenuContainer: FC = () => {
   const [email, setMail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
-  const isLogin = true
+  const isLogin = useSelector(
+    (state: RootState) => state.loginReducer.state
+  )
 
   return (
     <UserMenu
