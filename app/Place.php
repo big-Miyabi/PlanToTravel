b5f47546e8fd42e00cd14e98e8362c62f521450d
so-so-so-so <57979322+so-so-so-so@users.noreply.github.com>
2020-10-19 17:44:46 +0900

    Add:postcontrollerにplace項目の追加 タグ複数追加機能 Fix:ID取得をわかりやすいように変更

--- app/Place.php
+++ app/Place.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Place extends Model
 {
-    //
+  //
+  protected $fillable = [
+    'order_number', 'day', 'img', 'place_name', 'longitude', 'latitude', 'rating', 'weather', 'transport', 'transport_detail', 'distance', 'comment',
+  ];
+  public function schedules_places()
+  {
+    return $this->hasMany('App\Schedules_place');
+  }
 }
--- app/Schedules_place.php
+++ app/Schedules_place.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Schedules_place extends Model
 {
-    //
+  //
+  protected $fillable = [
+    'schedule_id', 'place_id'
+  ];
+  public function schedule()
+  {
+    return $this->belongsTo('App\Schedule');
+  }
+  public function place()
+  {
+    return $this->belongsTo('App\Place');
+  }
 }
