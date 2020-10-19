485193571eaac26755bfc8514895d3956985f270
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-19 11:57:06 +0900

    Add: testでPopularCardコンポーネントを作成

---
+++ resources/ts/src/components/molecules/PopularCard.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+const PopularCard: FC = () => {
+  return (
+    <div className="popular-card">
+      <p>test</p>
+    </div>
+  )
+}
+
+export default PopularCard
--- resources/ts/src/components/organisms/PopularPosts.tsx
+++ resources/ts/src/components/organisms/PopularPosts.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react'
+import PopularCard from '../../containers/molecules/PopularCard'
 
 const PopularPosts: FC = () => {
   return (
     <div className="popular-posts">
       <p className="popular-posts__title">人気の投稿</p>
+      <PopularCard />
     </div>
   )
 }
