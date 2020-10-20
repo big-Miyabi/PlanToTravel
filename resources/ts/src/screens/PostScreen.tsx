830536ec0953c9e80f6327582ca2bb3aeb1ab91e
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-20 12:33:25 +0900

    Add: プログレスバーのハリボテを追加

--- resources/ts/src/screens/PostScreen.tsx
+++ resources/ts/src/screens/PostScreen.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from 'react'
 import Header from '../containers/organisms/Header'
 import Menu from '../components/organisms/Menu'
+import ProgressBar from '../components/molecules/ProgressBar'
 
 const PostScreen: FC = () => {
   return (
     <div className="post-screen">
       <Header isPost={false} />
       <Menu />
+      <ProgressBar />
     </div>
   )
 }
