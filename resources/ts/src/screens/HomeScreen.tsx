1a65f6c7ba6e65430c16ed129c6313b3d929926e
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-21 10:14:11 +0900

    Add: ホーム画面右下に投稿ボタンを設置

--- resources/ts/src/screens/HomeScreen.tsx
+++ resources/ts/src/screens/HomeScreen.tsx
@@ -1,17 +1,28 @@
 import React, { FC } from 'react'
+import { RouteComponentProps as Props } from 'react-router-dom'
+import { colors } from '../utilities/colors'
 import Header from '../containers/organisms/Header'
 import Menu from '../components/organisms/Menu'
 import PopularPosts from '../containers/organisms/PopularPosts'
 import NewPosts from '../containers/organisms/NewPosts'
+import MapIcon from '../components/atoms/svg/MapIcon'
 
-const HomeScreen: FC = () => {
+const HomeScreen: FC<Props> = (props) => {
+  const onClick = () => {
+    props.history.push('../post')
+  }
+
   return (
     <div className="home-screen">
       <Header />
       <Menu />
       <PopularPosts />
       <NewPosts />
-      {/* <PostBtn /> */}
+      <MapIcon
+        className="home-screen__map-icon"
+        color={colors.yellow}
+        onClick={onClick}
+      />
     </div>
   )
 }
