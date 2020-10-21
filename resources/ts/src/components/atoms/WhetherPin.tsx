fd431ac28b551e9f0e548cca593ff146de18f0e8
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-21 10:12:55 +0900

    Add: 行程表のマップアイコンのSvgにグラデーションをかける処理を途中まで記述

--- resources/ts/src/components/atoms/WhetherPin.tsx
+++ resources/ts/src/components/atoms/WhetherPin.tsx
@@ -4,42 +4,78 @@ import CloudyPin from './svg/CloudyPin'
 import RainyPin from './svg/RainyPin'
 import SnowyPin from './svg/SnowyPin'
 import MoonyPin from './svg/MoonyPin'
+import { OpacityGradientType } from '../../utilities/types'
 
 type Props = {
   className: string
   whether: string
+  index?: number
 }
 
-const WhetherPin: FC<Props> = ({ className, whether }) => {
+const WhetherPin: FC<Props> = ({
+  className,
+  whether,
+  index,
+}) => {
+  const opacity: OpacityGradientType[] = [
+    {
+      start: 1,
+      end: 0.8421,
+    },
+    {
+      start: 0.6352,
+      end: 0.4774,
+    },
+    {
+      start: 0.2711,
+      end: 0.1132,
+    },
+  ]
+
   switch (whether) {
     case 'sun':
       return (
         <SunnyPin
           className={className + ' ' + 'whether-pin'}
+          opacityGradient={
+            index !== undefined ? opacity[index] : undefined
+          }
         />
       )
     case 'cloud':
       return (
         <CloudyPin
           className={className + ' ' + 'whether-pin'}
+          opacityGradient={
+            index !== undefined ? opacity[index] : undefined
+          }
         />
       )
     case 'rain':
       return (
         <RainyPin
           className={className + ' ' + 'whether-pin'}
+          opacityGradient={
+            index !== undefined ? opacity[index] : undefined
+          }
         />
       )
     case 'snow':
       return (
         <SnowyPin
           className={className + ' ' + 'whether-pin'}
+          opacityGradient={
+            index !== undefined ? opacity[index] : undefined
+          }
         />
       )
     default:
       return (
         <MoonyPin
           className={className + ' ' + 'whether-pin'}
+          opacityGradient={
+            index !== undefined ? opacity[index] : undefined
+          }
         />
       )
   }
--- resources/ts/src/components/atoms/svg/MoonyPin.tsx
+++ resources/ts/src/components/atoms/svg/MoonyPin.tsx
@@ -1,11 +1,21 @@
 import React, { FC } from 'react'
 import { colors } from '../../../utilities/colors'
+import { OpacityGradientType } from '../../../utilities/types'
 
 type Props = {
   className: string
+  opacityGradient?: OpacityGradientType
 }
 
-const MoonyPin: FC<Props> = ({ className }) => {
+const initialOpacity: OpacityGradientType = {
+  start: 1,
+  end: 1,
+}
+
+const MoonyPin: FC<Props> = ({
+  className,
+  opacityGradient = initialOpacity,
+}) => {
   return (
     <svg
       className={className}
@@ -15,9 +25,23 @@ const MoonyPin: FC<Props> = ({ className }) => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
+      <defs>
+        <linearGradient id="g1" x1="0" x2="0" y1="0" y2="1">
+          <stop
+            offset="0%"
+            stopColor={colors.navyBlue}
+            stopOpacity={opacityGradient.start}
+          ></stop>
+          <stop
+            offset="100%"
+            stopColor={colors.navyBlue}
+            stopOpacity={opacityGradient.end}
+          ></stop>
+        </linearGradient>
+      </defs>
       <path
         d="M17.4164 53C17.4164 53 -8.58891 26.4976 2.95461 7.57008C2.95461 7.57008 6.80405 0 18.3476 0C18.3476 0 33.7405 0 37.5852 15.1449C41.4298 30.2898 17.4164 53 17.4164 53Z"
-        fill={colors.navyBlue}
+        fill="url(#g1)"
       />
       <path
         d="M20.0693 31C23.1766 31 26.0086 29.4961 27.8966 26.9609C28.1719 26.5742 27.8573 26.0156 27.4246 26.1016C22.5473 27.1328 18.0633 23.0508 18.0633 17.6367C18.0633 14.543 19.558 11.707 22.036 10.1602C22.4293 9.90234 22.3113 9.30078 21.8786 9.21484C21.2886 9.08594 20.6986 9.04297 20.0693 9C14.484 9 10 13.9414 10 20C10 26.1016 14.484 31 20.0693 31Z"
