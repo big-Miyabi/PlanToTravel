074c2a8c15a2a700af5dfa63ce578c1210d785ff
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-22 16:41:28 +0900

    Add: GoogleMapを表示する画面を追加

--- resources/ts/src/containers/screens/MapScreen.tsx
+++ resources/ts/src/containers/screens/MapScreen.tsx
@@ -1,8 +1,15 @@
 import React, { FC } from 'react'
+import { Coords } from 'google-map-react'
 import MapScreen from '../../components/screens/MapScreen'
 
 const MapScreenContainer: FC = () => {
-  return <MapScreen />
+  const center: Coords = {
+    lat: 36.41032863354583,
+    lng: 138.34662204985045,
+  }
+  const zoom = 4.8
+
+  return <MapScreen center={center} zoom={zoom} />
 }
 
 export default MapScreenContainer
