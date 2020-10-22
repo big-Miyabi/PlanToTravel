4a8e0b5e1ec9c9ffff844b9dfb9c40d1d312367e
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-22 16:41:28 +0900

    Add: GoogleMapを表示する画面を追加

--- resources/ts/src/App.tsx
+++ resources/ts/src/App.tsx
@@ -10,6 +10,7 @@ import persistedReducer, { RootState } from './reducers'
 import RegisterScreen from './components/screens/RegisterScreen'
 import HomeScreen from './components/screens/HomeScreen'
 import PostScreen from './containers/screens/PostScreen'
+import MapScreen from './containers/screens/MapScreen'
 
 const App: FC = () => {
   return (
@@ -35,6 +36,7 @@ const App: FC = () => {
         exact
         component={PostScreen}
       />
+      <Route path={'/map'} exact component={MapScreen} />
     </BrowserRouter>
   )
 }
--- resources/ts/src/components/screens/MapScreen.tsx
+++ resources/ts/src/components/screens/MapScreen.tsx
@@ -1,9 +1,24 @@
 import React, { FC } from 'react'
+import GoogleMapReact, { Coords } from 'google-map-react'
 
-const MapScreen: FC = () => {
+type Props = {
+  center: Coords
+  zoom: number
+}
+
+const MapScreen: FC<Props> = ({ center, zoom }) => {
   return (
     <div className="map-screen">
-      <p>hogehoge</p>
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: `${process.env.MIX_GOOGLE_MAPS_API_KEY}`,
+        }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+        onClick={(v) => {
+          console.log(v)
+        }}
+      />
     </div>
   )
 }
