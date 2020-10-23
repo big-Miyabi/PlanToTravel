85a140f135cf656c5ce5ec42859d41a528e93090
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-23 15:05:06 +0900

    Add: googlemapsの型定義ファイルのインストール、検索機能のテスト

--- resources/ts/src/containers/screens/MapScreen.tsx
+++ resources/ts/src/containers/screens/MapScreen.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useState } from 'react'
-import { Coords } from 'google-map-react'
+import React, { FC, useEffect, useState } from 'react'
+import { Coords, Props } from 'google-map-react'
 import MapScreen from '../../components/screens/MapScreen'
+/// <reference types="googlemaps" />
 
 const MapScreenContainer: FC = () => {
   const center: Coords = {
@@ -11,12 +12,28 @@ const MapScreenContainer: FC = () => {
   const [location, setLocation] = useState<Coords>(center)
   const zoom = 15 // 4.8にすると日本全体が見える
 
+  const initGeocoder: Props['onGoogleApiLoaded'] = ({
+    maps,
+  }) => {
+    const Geocoder: google.maps.Geocoder = new maps.Geocoder()
+    const place = '国会議事堂'
+    Geocoder.geocode(
+      {
+        address: place,
+      },
+      (results, status) => {
+        console.log(results)
+      }
+    )
+  }
+
   return (
     <MapScreen
       center={center}
       zoom={zoom}
       location={location}
       setLocation={setLocation}
+      initGeocoder={initGeocoder}
     />
   )
 }
