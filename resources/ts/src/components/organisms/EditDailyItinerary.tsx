6031b63171e40a7016a5c072a6a3087f36c12130
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-26 10:26:10 +0900

    Add: EditDailyItineraryを追加した

---
+++ resources/ts/src/components/organisms/EditDailyItinerary.tsx
@@ -0,0 +1,51 @@
+import React, { FC } from 'react'
+import * as H from 'history'
+import { colors } from '../../utilities/colors'
+import MapIcon from '../atoms/svg/MapIcon'
+import FontAwesomeIconBtn from '../atoms/FontAwesomeIconBtn'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
+type Props = {
+  className: string
+  history: H.History
+  showMap: () => void
+  date: string
+  index: number
+}
+
+const EditDailyItinerary: FC<Props> = ({
+  className,
+  history,
+  showMap,
+  date,
+  index,
+}) => {
+  console.log(index)
+
+  return (
+    <div className="edit-itinerary__itinerary-for-the-day">
+      <div className="edit-itinerary__date-wrap">
+        <div className="edit-itinerary__date-border"></div>
+        <p className="edit-itinerary__date">{date}</p>
+      </div>
+      <div
+        className="edit-itinerary__add-place"
+        onClick={showMap}
+      >
+        <MapIcon
+          className="edit-itinerary__map-icon"
+          shouldHavePlus={false}
+          color={colors.navyBlue}
+        />
+        <p className="edit-itinerary__p">場所を追加</p>
+        <FontAwesomeIconBtn
+          className="edit-itinerary__plus"
+          icon={faPlus}
+          onClick={showMap}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default EditDailyItinerary
