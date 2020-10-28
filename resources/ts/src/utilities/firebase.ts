8edf2fde46598df22cb42ba8a9cded0dca2c586b
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-28 16:45:37 +0900

    Add: ヘッダー画像のアップロード処理と、場所の画像を全てアップロードしてurlを取得する処理を追加

--- resources/ts/src/utilities/firebase.ts
+++ resources/ts/src/utilities/firebase.ts
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.MIX_FIREBASE_API_KEY,
+  authDomain: process.env.MIX_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.MIX_FIREBASE_DATABASE_URL,
+  projectId: process.env.MIX_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.MIX_FIREBASE_STORAGE_BUCKET,
   messagingSenderId:
-    process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+    process.env.MIX_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.MIX_FIREBASE_APP_ID,
 }
 
 const firebaseApp = !firebase.apps.length
