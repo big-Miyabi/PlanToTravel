17c6a375d582ffa5774501327d57ebeace86ae4f
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-28 12:34:29 +0900

    Add: 概要入力画面にバリデーションチェックを追加

--- resources/ts/src/components/organisms/PostLocation.tsx
+++ resources/ts/src/components/organisms/PostLocation.tsx
@@ -1,4 +1,4 @@
-import React, { FC, Dispatch } from 'react'
+import React, { FC } from 'react'
 import { Place } from '../../utilities/types'
 import FormBtn from '../atoms/FormBtn'
 import moment from 'moment'
