41121c1b94b40d0c2d7792f6e6f14153de6b1811
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-28 10:01:25 +0900

    Add: 確認画面のヘッダー画像にタイトルなどを表示できるようにした

--- resources/ts/src/containers/molecules/PostImage.tsx
+++ resources/ts/src/containers/molecules/PostImage.tsx
@@ -1,8 +1,9 @@
 import React, { FC } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setImageSrc } from '../../actions/post'
-import PostImage from '../../components/molecules/PostImage'
+import { RootState } from '../../reducers'
 import { useFileInput } from '../../utilities/customHook'
+import PostImage from '../../components/molecules/PostImage'
 
 const PostImageContainer: FC = () => {
   const dispatch = useDispatch()
@@ -11,12 +12,29 @@ const PostImageContainer: FC = () => {
       dispatch(setImageSrc(src))
     }
   )
+  const { username, icon } = useSelector(
+    (state: RootState) => state.loginReducer
+  )
+  const progressIndex = useSelector(
+    (state: RootState) => state.postReducer.progressIndex
+  )
+  const { title, dateS, dateF, people, tags } = useSelector(
+    (state: RootState) => state.postReducer
+  )
 
   return (
     <PostImage
       inputRef={inputRef}
       src={src}
       onFileChange={onFileChange}
+      isConfirm={progressIndex === 2}
+      icon={icon}
+      username={username}
+      title={title}
+      dateS={dateS}
+      dateF={dateF}
+      people={people}
+      tags={tags}
     />
   )
 }
