80a3307366c91f8aa497c96ce834e71c3b39a0d1
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-29 14:09:32 +0900

    Add: メモリリークを防止する処理の追加

--- resources/ts/src/utilities/firebase.ts
+++ resources/ts/src/utilities/firebase.ts
@@ -1,4 +1,7 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: process.env.MIX_FIREBASE_API_KEY,
