9ec7dd02b16b8550fb1d84220e5805c3522a718a
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-30 10:12:51 +0900

    Rename: PostImageの命名が不適切と感じたためItineraryFirstViewに変更した(コードのみ)

--- resources/ts/src/components/screens/ItineraryDetailScreen.tsx
+++ resources/ts/src/components/screens/ItineraryDetailScreen.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from 'react'
+import Header from '../../containers/organisms/Header'
+import Menu from '../../components/organisms/Menu'
 
 const ItineraryDetailScreen: FC = () => {
-  return <div className="itinerary-detail"></div>
+  return (
+    <div className="itinerary-detail">
+      <Header isPost={true} />
+      <Menu />
+    </div>
+  )
 }
 
 export default ItineraryDetailScreen
