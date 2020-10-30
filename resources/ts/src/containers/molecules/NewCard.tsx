437efd417ac1909b7e0df1e7ace68d39ae4ce721
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-10-30 12:03:32 +0900

    Add: NewCardコンテナに少しだけ処理を記述

--- resources/ts/src/containers/molecules/NewCard.tsx
+++ resources/ts/src/containers/molecules/NewCard.tsx
@@ -1,6 +1,9 @@
-import React, { FC } from 'react'
-import NewCard from '../../components/molecules/NewCard'
+import React, { FC, useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../reducers'
+import { postByAxios } from '../../utilities/axios'
 import { PostCardType } from '../../utilities/types'
+import NewCard from '../../components/molecules/NewCard'
 
 type Props = {
   post: PostCardType
@@ -11,7 +14,51 @@ const NewCardContainer: FC<Props> = ({
   post,
   gradientId,
 }) => {
-  return <NewCard post={post} gradientId={gradientId} />
+  const myUid = useSelector(
+    (state: RootState) => state.loginReducer.id
+  )
+  const [isMeLover, setIsMeLover] = useState<boolean>(false)
+  const [isMeBookmarked, setIsMeBookmarked] = useState<
+    boolean
+  >(false)
+  const [likes, setLikes] = useState<number>(0)
+
+  useEffect(() => {
+    console.log(post.isBookmarked)
+    setIsMeLover(post.isLiked)
+    setIsMeBookmarked(post.isBookmarked)
+    setLikes(post.isLiked ? post.likes - 1 : post.likes)
+  }, [post.isBookmarked, post.isLiked, post.likes])
+
+  const onClickHeart = async () => {
+    setIsMeLover(!isMeLover)
+    const result = await postByAxios.like({
+      uid: String(myUid),
+      sid: String(post.id),
+    })
+    console.log(result.result)
+  }
+
+  const onClickBookmark = async () => {
+    setIsMeBookmarked(!isMeBookmarked)
+    const result = await postByAxios.bookmark({
+      uid: String(myUid),
+      sid: String(post.id),
+    })
+    console.log(result.result)
+  }
+
+  return (
+    <NewCard
+      post={post}
+      gradientId={gradientId}
+      isMeLover={isMeLover}
+      isMeBookmarked={isMeBookmarked}
+      likes={likes}
+      onClickHeart={onClickHeart}
+      onClickBookmark={onClickBookmark}
+    />
+  )
 }
 
 export default NewCardContainer
